a07eb7a536977704ad89a91276c3789a82167f52
heebm2 <michael.heeb@students.bfh.ch>
2014-11-25 11:18:51 +0100

    no message

--- app/src/main/java/com/example/pzehnder/diabetesbuddy/Login.java
+++ app/src/main/java/com/example/pzehnder/diabetesbuddy/Login.java
@@ -48,12 +48,7 @@ public class Login extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
